301a111f25ae4244d1f7f485fe999c6ab57b4850
Bhandari <SBhandari@globalindustrial.com>
2025-10-13 19:29:08 +0530

    Updated the code for mobile application

--- Components/navbar.jsx
+++ Components/navbar.jsx
@@ -1,18 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
+import Hamburger from "../public/image.png"
+import { useState } from "react";
 
 export default function Navbar(){
-
+    const [nav,setNav] = useState(false);
     return(
         <>
-        <div className="flex py-3 flex-wrap justify-around">
+        <div className="flex py-3 flex-wrap justify-around mainPage">
             <h1 className="text-lg font-semibold">Todo App</h1>
-            <ul className="flex gap-[40px] text-m">
+            <ul className="flex gap-[40px] text-m navWeb">
                 <li>Home</li>
                 <li>ABout</li>
                 <li><Link href="/Products">Products</Link> </li>
                 <li>Contact</li>
             </ul>
+            <Image src={Hamburger} className="w-[5%] h-[5%] navMobile" onClick={() => nav === true ? setNav(false) : setNav(true)} alt="hamburger"/>
         </div>
+        {nav && (
+                <ul className="gap-[10px] grid text-right text-white bg-orange-600 w-[25%] px-2 py-2 ml-[70%] absolute justify-end">
+                <li>Home</li>
+                <li>About</li>
+                <li><Link href="/Products">Products</Link> </li>
+                <li>Contact</li>
+            </ul>
+            )}
         </>
     )
 } 
